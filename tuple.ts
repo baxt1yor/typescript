2b04be2602cb77e3b4de599325603cacdbbe734d
Baxtiyor Eshametov <52046670+baxt1yor@users.noreply.github.com>
2023-06-05 14:58:44 +0500

    Update tuple.ts

--- tuple.ts
+++ tuple.ts
@@ -3,4 +3,6 @@ const tuple = <T extends any[]>(...args: T): T => args
 
 let d = tuple("hello", 1, true);
 
+let testTuple: readonly [string, number, boolean] = ['Age', 24, true] 
+
 console.log(d);
